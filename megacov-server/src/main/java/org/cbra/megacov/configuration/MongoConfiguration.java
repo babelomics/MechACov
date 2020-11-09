ed0759149fe49f9f00927d24e9c0013d7d01577e
José LF Rueda <josel.fernandez.rueda@juntadeandalucia.es>
2020-11-09 17:10:41 +0100

    added web services scaffold

---
+++ megacov-server/src/main/java/org/cbra/megacov/configuration/MongoConfiguration.java
@@ -0,0 +1,54 @@
+package org.cbra.megacov.configuration;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.lang.NonNull;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories(basePackages = "org.babelomics.mmp.data")
+
+public class MongoConfiguration  extends AbstractMongoClientConfiguration {
+
+    private final static String ADMIN_DB_NAME = "admin";
+
+    @Value("${mongo.host}")
+    private String host;
+
+    @Value("${mongo.port}")
+    private Integer port;
+
+    @Value("${mongo.user}")
+    private String username;
+
+    @Value("${mongo.password}")
+    private String password;
+
+    @Value("${mongo.db.name}")
+    private String databaseName;
+
+    @Override
+    @NonNull
+    public MongoClient mongoClient() {
+        var connString = new ConnectionString(String.format("mongodb://%s:%s", host, port));
+        var settingsBuilder = MongoClientSettings.builder().applyConnectionString(connString);
+        if (StringUtils.isNotBlank(username)) {
+            var credential = MongoCredential.createCredential(username, ADMIN_DB_NAME, password.toCharArray());
+            settingsBuilder.credential(credential);
+        }
+        return MongoClients.create(settingsBuilder.build());
+    }
+
+
+    @Override
+    protected String getDatabaseName() {
+        return databaseName;
+    }
+}
