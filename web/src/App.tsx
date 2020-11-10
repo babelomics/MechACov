14ed0441cbf9fa32dec50a3a2f83ff855ba765ee
José LF Rueda <josel.fernandez.rueda@juntadeandalucia.es>
2020-11-10 17:18:48 +0100

    updating web project

---
+++ web/src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import 'typeface-roboto';
+
+//components
+import MenuToolbar from './modules/components/menuToolbar/MenuToolbar'
+import Footer from './modules/components/footer/Footer'
+
+//Router
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+//Routes
+import { routesPublic } from './modules/layout/routes'
+
+function App() {
+
+  const routeComponentsPublic = routesPublic
+    .map(({ path, component }, key) =>
+      <Route
+        exact path={path}
+        component={component}
+        key={key} />);
+
+  return (
+    <Router>
+      <MenuToolbar />
+      <Switch>
+        {routeComponentsPublic}
+      </Switch>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
