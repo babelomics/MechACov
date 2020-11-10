642f596fbc5aea20ac3d555ecba7e31e5751d695
Daniel López López <daniel.lopez.lopez@juntadeandalucia.es>
2020-11-10 13:38:15 +0100

    add backbone scripts

---
+++ src/03_Expression2DifferentialExpression.R
@@ -0,0 +1,3 @@
+# intput: Gene expression normalized and annotated file (02a_GeneAnnotation.R) or hipathia path_vals annotated file (02b_CircuitAnnotation.R)
+# output: genes/circuits differentially expressed file
+# output format: Id (gene/circuit), displayName (human-like short description), LogFC, pvalue-adj, t, B]
---
+++ src/04b_MetaAnalysis_MetaMA.R
@@ -0,0 +1,4 @@
+# input: Array of Genes/circuits differentially expressed files (from 02_GeneCircuitExpression2DifferentialExpression.R)
+# output: Combined list of genes/circuits differentially expressed file
+# output format: id, displayName, Effect/pvalue, [confidentInterval]
+
