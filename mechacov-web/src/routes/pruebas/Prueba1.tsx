d58c60833ff9b4616bfe7d1dc590797842a3383d
Rubén García Serrano <onitaku@gmail.com>
2020-11-11 18:46:24 +0100

    mechacov-web visx test

---
+++ mechacov-web/src/routes/pruebas/Prueba1.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {  Grid } from '@material-ui/core';
+import HistogramBar from '../visualizations/HistogramBar';
+import Heatmap from '../../components/Heatmap';
+
+
+function Prueba1() {
+    return (
+        <Grid container
+            direction="column"
+            justify="space-evenly"
+            alignItems="center">
+            <Grid item sm={12}>
+                <br></br>
+                 <HistogramBar width={640} height={480} />
+            </Grid>
+            <Grid item sm={12}>
+                <br></br>
+                 <Heatmap width={640} height={480} />
+            </Grid>
+        </Grid>
+    );
+}
+
+export default Prueba1;
