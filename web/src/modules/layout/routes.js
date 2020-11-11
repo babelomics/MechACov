8c57f297676717805102ceca5530371e8c9f2679
Rubén García Serrano <onitaku@gmail.com>
2020-11-11 11:53:38 +0100

    web updates

--- web/src/modules/layout/routes.js
+++ web/src/modules/layout/routes.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import Home from './Home'
+import Explore from './Explore'
+
 import About from './About'
 
 import HomeIcon from '@material-ui/icons/HomeTwoTone';
+import ExploreIcon from '@material-ui/icons/BarChartTwoTone';
 import AboutIcon from '@material-ui/icons/InfoTwoTone';
 
 export const routesPublic = [
@@ -12,6 +15,13 @@ export const routesPublic = [
         component: Home,
         menuList: true,
         icon: <HomeIcon />
+    },
+    {
+        name: "Explore Datasets",
+        path: '/explore',
+        component: Explore,
+        menuList: true,
+        icon: <ExploreIcon />,
     },
     {
         name: "About",
