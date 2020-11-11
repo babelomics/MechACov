8be7d1a3c70a1ca02a9be239f6389df506ec0127
José LF Rueda <josel.fernandez.rueda@juntadeandalucia.es>
2020-11-11 19:05:47 +0100

    experiment controller scaffold in server

--- mechacov-server/src/main/java/org/cbra/megacov/services/ExperimentService.java
+++ mechacov-server/src/main/java/org/cbra/megacov/services/ExperimentService.java
@@ -1,2 +1,17 @@
-package org.cbra.megacov.services;public class ExperimentService {
+package org.cbra.megacov.services;
+
+import org.cbra.megacov.entities.Experiment;
+import org.cbra.megacov.models.ExperimentRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExperimentService {
+
+    public Experiment createExperiment(ExperimentRequest request) {
+        var experiment = new Experiment();
+        experiment.setControlIds(request.getControlIds());
+        experiment.setCaseIds(request.getCaseIds());
+        return experiment;
+    }
+
 }
