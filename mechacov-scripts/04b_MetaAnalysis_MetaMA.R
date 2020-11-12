7afed5451f9901e129eb30044bb33670f3e3ec8f
Daniel López López <daniel.lopez.lopez@juntadeandalucia.es>
2020-11-12 14:34:09 +0100

    don't use genes/circuits with NAs

--- mechacov-scripts/04b_MetaAnalysis_MetaMA.R
+++ mechacov-scripts/04b_MetaAnalysis_MetaMA.R
@@ -19,7 +19,7 @@ argv = parse_args(argp)
 #######################
 ###  safety checks  ###
 #######################
-for (argname in c("input")) {
+for (argname in c("input", "output")) {
   if (is.na(argv[argname]) || is.null(argv[argname])) {
     msg = paste0("Required argument missing: --", argname)
     write(msg, stderr())
@@ -43,10 +43,29 @@ if (is.na(argv$input) || is.null(argv$input) || !file.exists(argv$input)) {
 # studyId, id, displayName, logFC, pvalues
 experiment <- readRDS(file = argv$input)
 
+n <- c(1:length(experiment))
+esets <- list()
+classes <- list()
+ids <- Reduce(intersect,lapply(experiment, function(s){ return (rownames(s$matrix_data[complete.cases(s$matrix),])) }))
+
+for (e in n){
+  m <- experiment[[e]]$matrix_data
+  esets[[e]] <- m[rownames(m) %in% ids,]
+  classes[[e]] <- experiment[[e]]$group
+}
+
 # performs MetaMA analysis
-res=EScombination(esets=experiment$esets,classes=experiment$classes)
+res=EScombination(esets=esets,classes=classes)
 rawpval=2*(1-pnorm(abs(res$TestStatistic)))
 adjpval=p.adjust(rawpval, method = "BH")
+
+results.df <- data.frame (
+  id = experiment$geneNames[res$Meta],
+  effect = res$TestStatistic[res$Meta],
+  pval = adjpval[res$Meta]
+)
+
+write_tsv(results.df, file = argv$output)
 
 
 
