d8330d4bf8104081974c10999147a588cbe2811a
Daniel López López <daniel.lopez.lopez@juntadeandalucia.es>
2020-11-12 18:10:55 +0100

    remove unused scripts

--- mechacov-scripts/02a_GeneAnnotation.R
+++
@@ -1,4 +0,0 @@
-# input: Gene expression normalized file (downloaded from GEO/AE)
-# output: Annotated expression file
-# output format: geneId, displayName (human-like short description/name), Sample1, Sample2, [SampleN]
-# output example:
--- mechacov-scripts/02b_CircuitAnnotation.R
+++
@@ -1,3 +0,0 @@
-# input: Hipathia pathvals file (01_GeneExpressionNorm2PathVals.R)
-# output: Annotated expression file
-# output format: circuitId, displayName (human-like short description/name), Sample1, Sample2, [SampleN]
