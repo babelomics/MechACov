0eb53e0619a932779ef7b1dd578d02fbacd4aeab
Rubén García Serrano <onitaku@gmail.com>
2020-12-14 13:38:35 +0100

    ts data

---
+++ mechacov-web/src/data/website-visit-age-group.ts
@@ -0,0 +1,27 @@
+export const AGE_GROUP = [
+    {
+      age: "18-24",
+      percentage: 14
+    },
+    {
+      age: "25-34",
+      percentage: 24
+    },
+    {
+      age: "35-44",
+      percentage: 22
+    },
+    {
+      age: "45-54",
+      percentage: 18
+    },
+    {
+      age: "55-64",
+      percentage: 16
+    },
+    {
+      age: "65+",
+      percentage: 6
+    }
+  ];
+  
