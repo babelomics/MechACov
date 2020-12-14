d2a6ef3a2edf756b8944db8e344529e6ebfe8521
Rubén García Serrano <onitaku@gmail.com>
2020-12-14 13:38:23 +0100

    utils

---
+++ mechacov-web/src/utils/month-mapping.ts
@@ -0,0 +1,17 @@
+const MONTHS = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+];
+
+export const getMonthNameByOrder = (order: number): string | undefined =>
+    MONTHS[order - 1];
